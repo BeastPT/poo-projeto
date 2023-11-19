b0b31fa5e3d5c72b0071237a4d3483803133c6ef
BeastPT <ajfa2004@gmail.com>
2023-11-19 14:42:47 +0000

    project setup

---
+++ projeto-poo/src/main/java/pt/uma/projeto/poo/ProjetoPoo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package pt.uma.projeto.poo;
+
+/**
+ *
+ * @author afons
+ */
+public class ProjetoPoo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
