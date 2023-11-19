7f21589f36d927277f634329d2093f4f007d6a1a
BeastPT <ajfa2004@gmail.com>
2023-11-19 15:22:41 +0000

    update

--- projeto-poo/src/main/java/pt/uma/projeto/poo/ProjetoPoo.java
+++ projeto-poo/src/main/java/pt/uma/projeto/poo/ProjetoPoo.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package pt.uma.projeto.poo;
 
-/**
- *
- * @author afons
- */
 public class ProjetoPoo {
 
     public static void main(String[] args) {
